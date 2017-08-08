c71ba0af67a0832b37895969f67e5fb0f89a417a
Shashank Gaurav_17 <shashankgaurav17@gmail.com>
2017-08-08 12:46:00 +0530

    Added JS file for Rotate and modified Resize code

--- rotate.js
+++ rotate.js
@@ -1,158 +1,32 @@
-var minWidth = 60;
-var minHeight = 40;
-
-// Thresholds
-var MARGINS = 4;
-
-// End of what's configurable.
-var clicked = null;
-var onRightEdge, onBottomEdge, onLeftEdge, onTopEdge;
-
-var b, x, y;
-
-var redraw = false;
-
-var pane = document.getElementById('pane');
-
-var e;
-
-// Mouse events
-pane.addEventListener('mousedown', onMouseDown);
-document.addEventListener('mousemove', onMove);
-document.addEventListener('mouseup', onUp);
-
-// Touch events 
-pane.addEventListener('touchstart', onTouchDown);
-document.addEventListener('touchmove', onTouchMove);
-document.addEventListener('touchend', onTouchEnd);
-
-
-function onTouchDown(e) {
-  onDown(e.touches[0]);
-  e.preventDefault();
-}
-
-function onTouchMove(e) {
-  onMove(e.touches[0]);     
-}
-
-function onTouchEnd(e) {
-  if (e.touches.length ==0) onUp(e.changedTouches[0]);
-}
-
-function onMouseDown(e) {
-  onDown(e);
-  e.preventDefault();
-}
-
-function onMove(ee) {
-  calc(ee);
-  e = ee;
-  redraw = true;
-}
-
-function onUp(e) {
-  calc(e);
-  clicked = null;
-
-}
-
-function onDown(e) {
-  calc(e);
-
-  var isResizing = onRightEdge || onBottomEdge || onTopEdge || onLeftEdge;
-  console.log("isResizing: " + isResizing);
-
-  clicked = {
-	x: x,
-	y: y,
-	cx: e.clientX,
-	cy: e.clientY,
-	w: b.width,
-	h: b.height,
-	isResizing: isResizing,
-	onTopEdge: onTopEdge,
-	onLeftEdge: onLeftEdge,
-	onRightEdge: onRightEdge,
-	onBottomEdge: onBottomEdge
-  };
+$(document).ready(function() {
+    $(".grid-icon-margin a .butas-icon").on('click',function(){
+        $(this).toggleClass("butas-border");
+        $('#svgx').append('<svg class="layers" x="80" y="80" height="500" width="560"><image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="assets/images/peacock.PNG" class="draggable"  onmousedown="selectElement(evt)" transform="matrix(1 0 0 1 0 0)" height="67" width="67" x="280" y="280"></image></svg>');
+    });
   
-  console.log("clicked:" + clicked.x);
-}
+    $(".btn-select").on("click",function(){
+        $("#pane").toggleClass("isSelected");
+    });
 
+var count = 0;
+    $(".btn-rotate").on("click",function(){
+        if(count == 0){
+            $("#pane").css("transform","rotate(90deg)");
+            count++;
+        } 
+        else if (count == 1){
+            $("#pane").css("transform","rotate(180deg)");
+            count++;
+        }  
+        else if (count == 2){
+            $("#pane").css("transform","rotate(270deg)");
+            count++;
+        }
+        else {
+            $("#pane").css("transform","rotate(360deg)");
+            count=0;
+        }
+    });
+    
 
-function animate() {
-
-  requestAnimationFrame(animate);
-
-  if (!redraw) return;
-
-  redraw = false;
-
-  if (clicked && clicked.isResizing) {
-
-	if (clicked.onRightEdge) pane.style.width = Math.max(x, minWidth) + 'px';
-	if (clicked.onBottomEdge) pane.style.height = Math.max(y, minHeight) + 'px';
-
-	if (clicked.onLeftEdge) {
-	  var currentWidth = Math.max(clicked.cx - e.clientX  + clicked.w, minWidth);
-	  if (currentWidth > minWidth) {
-		pane.style.width = currentWidth + 'px';
-		pane.style.left = e.clientX + 'px'; 
-	  }
-	}
-
-	if (clicked.onTopEdge) {
-	  var currentHeight = Math.max(clicked.cy - e.clientY  + clicked.h, minHeight);
-	  if (currentHeight > minHeight) {
-		pane.style.height = currentHeight + 'px';
-		pane.style.top = e.clientY + 'px';  
-	  }
-	}
-
-
-	return;
-  }
-
-  // This code executes when mouse moves without clicking
-
-  // style cursor
-  if (onRightEdge && onBottomEdge || onLeftEdge && onTopEdge) {
-	pane.style.cursor = 'nwse-resize';
-  } else if (onRightEdge && onTopEdge || onBottomEdge && onLeftEdge) {
-	pane.style.cursor = 'nesw-resize';
-  } else if (onRightEdge || onLeftEdge) {
-	pane.style.cursor = 'ew-resize';
-  } else if (onBottomEdge || onTopEdge) {
-	pane.style.cursor = 'ns-resize';
-  } else {
-	pane.style.cursor = 'default';
-  }
-}
-
-function calc(e) {
-  b = pane.getBoundingClientRect();
-  x = e.clientX - b.left;
-  y = e.clientY - b.top;
-
-  onTopEdge = y < MARGINS;
-  onLeftEdge = x < MARGINS;
-  onRightEdge = x >= b.width - MARGINS;
-  onBottomEdge = y >= b.height - MARGINS;
-  
-  console.log("e.clientX : " + e.clientX);
-  console.log("e.clientY: " + e.clientY);
-  console.log("b.left: " + b.left);
-  console.log("b.top: " + b.top);
-  
-  console.log("b: " + b);
-  console.log("x: " + x);
-  console.log("y: " + y);
-  console.log("onTopEdge: " + onTopEdge);
-  console.log("onLeftEdge: " + onLeftEdge);
-  console.log("onRightEdge: " + onRightEdge);
-  console.log("onBottomEdge: " + onBottomEdge);
-  console.log("****************");
-}
-
-animate();
+});	    
