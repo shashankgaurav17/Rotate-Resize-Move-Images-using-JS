de28aae07e567b5de686428f8d6481e2961d047f
Shashank Gaurav_17 <shashankgaurav17@gmail.com>
2017-08-30 14:46:25 +0530

    minor changes

--- rotate.js
+++ rotate.js
@@ -1,15 +1,8 @@
 $(document).ready(function() {
-    $(".grid-icon-margin a .butas-icon").on('click',function(){
-        $(this).toggleClass("butas-border");
-        $('#svgx').append('<svg class="layers" x="80" y="80" height="500" width="560"><image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="assets/images/peacock.PNG" class="draggable"  onmousedown="selectElement(evt)" transform="matrix(1 0 0 1 0 0)" height="67" width="67" x="280" y="280"></image></svg>');
-    });
-  
-    $(".btn-select").on("click",function(){
-        $("#pane").toggleClass("isSelected");
-    });
 
 var count = 0;
-    $(".btn-rotate").on("click",function(){
+    $("#rotate_right").on("click",function(){
+		//$("#pane").css("transform","rotate(90deg)");
         if(count == 0){
             $("#pane").css("transform","rotate(90deg)");
             count++;
@@ -25,6 +18,27 @@ var count = 0;
         else {
             $("#pane").css("transform","rotate(360deg)");
             count=0;
+        }
+    });
+	
+var count1 = 0;
+    $("#rotate_left").on("click",function(){
+		//$("#pane").css("transform","rotate(90deg)");
+        if(count1 == 0){
+            $("#pane").css("transform","rotate(-90deg)");
+            count1++;
+        } 
+        else if (count1 == 1){
+            $("#pane").css("transform","rotate(-180deg)");
+            count1++;
+        }  
+        else if (count1 == 2){
+            $("#pane").css("transform","rotate(-270deg)");
+            count1++;
+        }
+        else {
+            $("#pane").css("transform","rotate(-360deg)");
+            count1=0;
         }
     });
     
